a693cbc31d2a0cbf518a81ce0b91dbdf1c75a27f
Drew DeCarme <drewdecarme@gmail.com>
2025-05-19 10:09:04 -0400

    some updates

--- packages/core-sandbox/scripts/build-sandbox-cli-with-fizmoo.ts
+++ packages/core-sandbox/scripts/build-sandbox-cli-with-fizmoo.ts
@@ -7,7 +7,9 @@ async function buildSandboxCLIWithFizmoo() {
     const fizmoo = await createFizmoo({
       logLevel: "trace",
       env: "development",
+      autoInit: true,
     });
+    if (!fizmoo) return;
     await fizmoo.build();
     LOG.success("Building the Sandbox CLI with Fizmoo... done.");
   } catch (error) {
